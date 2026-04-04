c3c98c7f7a34f76d2af0eeea80f05aa2aaa887e8
German <german@german.com>
2026-04-04 20:22:39 +0300

    Добавлен проект ThreadsLab

---
+++ ThreadsLab/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ThreadsLab
+{
+    class Program
+    {
+        static void WriteSecond()
+        {
+            while (true)
+            {
+                Console.WriteLine(new String(' ', 10) + "Вторичный");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadStart writeSecond = new ThreadStart(WriteSecond);
+            Thread thread = new Thread(writeSecond);
+            thread.Start();
+
+            while (true)
+            {
+                Console.WriteLine("Первичный");
+            }
+        }
+    }
+}
