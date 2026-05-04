8691184f306fa95ca3aca664f5ddb0bb0aa9c913
German <german@german.com>
2026-05-04 22:58:19 +0300

    доработка табов в wpf

--- WPFScrutiniLab/MainWindow.xaml.cs
+++ WPFScrutiniLab/MainWindow.xaml.cs
@@ -20,24 +20,49 @@ namespace WPFScrutiniLab
         {
             InitializeComponent();
 
+
+
             #region Пример добавления табов в C#
 
-            // формируем содержимое вкладки в виде списка
-            ListBox notesList = new ListBox();
-            notesList.Items.Add("Macbook Pro");
-            notesList.Items.Add("HP Pavilion 5478");
-            notesList.Items.Add("Acer LK-08");
-            // добавление вкладки
+            //// формируем содержимое вкладки в виде списка
+            //ListBox notesList = new ListBox();
+            //notesList.Items.Add("Macbook Pro");
+            //notesList.Items.Add("HP Pavilion 5478");
+            //notesList.Items.Add("Acer LK-08");
+            //// добавление вкладки
             
+            //products.Items.Add(new TabItem
+            //{
+            //    Header = new TextBlock { Text = "Ноутбуки" }, // установка заголовка вкладки
+            //    Content = notesList // установка содержимого вкладки
+            //});
+
+            #endregion Пример добавления табов в C#
+        }
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			// формируем содержимое вкладки в виде списка
+			ListBox notesList = new ListBox();
+			notesList.Items.Add("Macbook Pro");
+			notesList.Items.Add("HP Pavilion 5478");
+			notesList.Items.Add("Acer LK-08");
+            // добавление вкладки
+
             products.Items.Add(new TabItem
             {
                 Header = new TextBlock { Text = "Ноутбуки" }, // установка заголовка вкладки
                 Content = notesList // установка содержимого вкладки
             });
-
-            #endregion Пример добавления табов в C#
         }
 
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+			products.Items.RemoveAt(products.SelectedIndex);
 
-    }
+			TabItem current = (TabItem)products.SelectedItem;    
+
+		//	current.Visibility = Visibility.Collapsed;
+		}
+	}
 }
