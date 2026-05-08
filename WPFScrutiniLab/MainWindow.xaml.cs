4473cc3cb288405c80bd6fe6fa86b6f6c40bf9cb
Vitaly <vitaly@example.com>
2026-05-08 18:32:52 +0300

    доработка табов 4

--- WPFScrutiniLab/MainWindow.xaml.cs
+++ WPFScrutiniLab/MainWindow.xaml.cs
@@ -17,45 +17,57 @@ namespace WPFScrutiniLab
     public partial class MainWindow : Window
     {
         //TabControl tabControl = new TabControl();
-        
 
+        StackPanel stackPanelTabItemHeader = new StackPanel();
+        Grid gridCentr = new Grid();
+        TabControl tabControl = new TabControl();
+        TabItem tabItemStart = new TabItem();
 
         public MainWindow()
         {
             InitializeComponent();
-            //AddTabItem();
+
+            gridCentr.Name = "GridCenter";
+            GridCenter.Children.Add(tabControl);
         }
 
-		private void AddTabItem(object sender, RoutedEventArgs e)
-		{
+        private void AddTabItem(object sender, RoutedEventArgs e)
+        {
             TabItem tabItem = new TabItem();
-            StackPanel stackPanel = new StackPanel();
+            StackPanel stackPanelTabItemHeader = new StackPanel();
+            // stackPanel.Name = "currentStackPanel";
             Ellipse ellipse = new Ellipse() { Height = 10, Width = 10, Fill = Brushes.Black };
             TextBlock textBlock = new TextBlock() { Margin = new Thickness(3), Text = "Программный Таб" };
 
-            stackPanel.Orientation = Orientation.Horizontal;
-            stackPanel.Children.Add(ellipse);
-            stackPanel.Children.Add(textBlock);
+            stackPanelTabItemHeader.Orientation = Orientation.Horizontal;
+            stackPanelTabItemHeader.Children.Add(ellipse);
+            stackPanelTabItemHeader.Children.Add(textBlock);
 
-            tabItem.Header = stackPanel;
+            tabItem.Header = stackPanelTabItemHeader;
 
+            tabControl.Visibility = Visibility.Visible;
             tabControl.Items.Add(tabItem);
             tabItem.IsSelected = true;
 
 
         }
 
-		private void DelTabItem(object sender, RoutedEventArgs e)
-		{
+        private void DelTabItem(object sender, RoutedEventArgs e)
+        {
             //products.Items.RemoveAt(products.SelectedIndex);
 
             TabItem current = (TabItem)tabControl.SelectedItem;
             current.Visibility = Visibility.Collapsed;
             tabItemStart.Visibility = Visibility.Collapsed;
+            // tabControl.Visibility = Visibility.Collapsed;
+            stackPanelTabItemHeader.Visibility = Visibility.Collapsed;
+
+
+
 
 
 
 
         }
-	}
+    }
 }
