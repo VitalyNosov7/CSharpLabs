ee85ed1dc2f0e6466a0851e2e2a82efca8d1818a
Vitaly <vitaly@example.com>
2026-05-11 18:45:58 +0300

    переработка структуры приложения

---
+++ WPFScrutiniLab/Views/Tabs/BookingTab.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+
+namespace WPFScrutiniLab.Views.Tabs
+{
+    public class BookingTab
+    {
+        StackPanel stackPanelTabItemHeader = new StackPanel();
+        Grid gridCentr = new Grid();
+        TabControl tabControl = new TabControl();
+        TabItem tabItemStart = new TabItem();
+
+        public BookingTab()
+        {
+            gridCentr = new Grid() { Name = "GridCenter" };
+            GridCenter.Children.Add(tabControl);
+        }
+    }
+}
